c17a0674a0e059dd63504b5726e8225ba792bd29
Jackson Gross <jackson.gross@gmail.com>
2019-01-25 16:52:55 +1100

    use function to allow overrides

--- javascript/README.md
+++ javascript/README.md
@@ -3,8 +3,17 @@
 This repo contains a custom Eslint config that is built upon the rules and configuration defined in [eslint-config-airbnb](https://github.com/airbnb/javascript) and eslint-plugin-vue, which follows the Official [Vue Style Guide](https://vuejs.org/v2/style-guide/) using the recommended rules.
 
 ## Installation
-TODO
+You will need the NPM registry auth token (found in 1password) in your environment to install this package.
+
+`npm i --save-dev @streemau/eslint-config-streem`
+
 
 ## Using eslint-config-streem
-TODO
+In an `.eslintrc.js` config file, simply do:
+``` javascript
+const config = {
+  // add any overrides or extra config here
+};
+module.exports = require('@streemau/eslint-config-streem')(config)
+```
 
--- javascript/eslint-config-streem/index.js
+++ javascript/eslint-config-streem/index.js
@@ -1,57 +1,54 @@
-module.exports = {
-    root: true,
-    parserOptions: {
-      parser: 'babel-eslint',
-      sourceType: 'module'
-    },
-    env: {
-      browser: true
-    },
-    extends: [
-      'airbnb',
-      'plugin:vue/recommended'
-    ],
-    // required to lint *.vue files
-    plugins: [
-      'vue'
-    ],
-    globals: {
-      'ga': true, // Google Analytics
-      'cordova': true,
-      '__statics': true
-    },
-    overrides: [
-      {
-        "files": ["shared/store/*/*.js", "src/store/*.js", "src/store/desktop/*.js", "src/store/shortcuts/*.js", "src/plugins/*.js", "shared/plugins/*.js"],
-        "rules": {
-          "no-param-reassign": "off"
-        }
-      }
-    ],
-    // add your custom rules here
-    rules: {
-      // allow async-await
-      'generator-star-spacing': 'off',
-  
-      'class-methods-use-this': 'off',
-  
-      // allow paren-less arrow functions
-      'arrow-parens': 0,
-      'one-var': 0,
-  
-      'import/first': 0,
-      'import/named': 2,
-      'import/namespace': 2,
-      'import/default': 2,
-      'import/export': 2,
-      'import/extensions': 0,
-      'import/no-unresolved': 0,
-      'import/no-extraneous-dependencies': 0,
-  
-      'semi': [2, 'always'],
-  
-      // allow debugger during development
-      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-    }
+const config = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  extends: [
+    'airbnb',
+    'plugin:vue/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  globals: {
+    'ga': true, // Google Analytics
+    'cordova': true,
+    '__statics': true
+  },
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+
+    'class-methods-use-this': 'off',
+
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    'one-var': 0,
+
+    'import/first': 0,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/no-extraneous-dependencies': 0,
+
+    'semi': [2, 'always'],
+
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
-  
+};
+
+const mergeConfig = (cfg = {}) => {
+  return Object.assign({}, config, cfg);
+};
+
+module.exports = mergeConfig;
